35dd7145ee3434a08be68b19df2c0c390b715ade
Felix Palmen <felix@palmen-it.de>
2026-02-16 10:19:00 +0100

    bhdr: Document the automatic BASIC header

--- src/bhdr.s
+++ src/bhdr.s
@@ -1,12 +1,19 @@
 .import __BHDR_LOAD__
 .import entry
 
+; BASIC header, including PRG load address
 .segment "BHDR"
-		.word	__BHDR_LOAD__+2
-		.word	hdrend
-		.word	2026
+		.word	__BHDR_LOAD__+2	; load address from linker config
+
+		.word	hdrend		; pointer to next BASIC line
+		.word	2026		; line number
+
+		; For the SYS command (token $9e), convert entry address
+		; to 4-digit decimal number in PETSCII
 		.byte	$9e, <((entry/1000) .mod 10)+$30
 		.byte	<((entry/100) .mod 10)+$30
-		.byte	<((entry/10) .mod 10)+$30, <(entry .mod 10)+$30, 0
-hdrend:		.word	0
+		.byte	<((entry/10) .mod 10)+$30, <(entry .mod 10)+$30
 
+		.byte	0		; <NUL>, end of BASIC line
+
+hdrend:		.word	0		; NULL pointer, end of BASIC program
